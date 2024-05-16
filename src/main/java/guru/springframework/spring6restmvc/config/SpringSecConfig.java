b92c563eaae28bd743a107ba337050d7909fd05f
saisourabh <sai.sourabh2@gmail.com>
2024-05-16 21:26:16 +0530

    239 - Spring Security Configuration

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
@@ -16,7 +16,11 @@ public class SpringSecConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
       //  http.csrf().ignoringRequestMatchers("/api/**");
-        return http.authorizeRequests().anyRequest().authenticated().and().httpBasic(Customizer.withDefaults())
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/**")).build();
+        return http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
     }
 }
